9babd43092f49e0cb98f8726ef75e3380607bd24
Vitold Kliain <kliain.tech@gmail.com>
2022-04-21 21:22:52 +0200

    Update views

--- app/main.py
+++ app/main.py
@@ -1,6 +1,4 @@
 from flask import Flask
-import os
-import datetime
 
 
 def create_app():
--- app/views.py
+++ app/views.py
@@ -1,6 +1,5 @@
-import os
-
 from flask import request, Blueprint, render_template
+
 
 greeting_bp = Blueprint('greeting', __name__)
 
@@ -8,7 +7,7 @@ greeting_bp = Blueprint('greeting', __name__)
 @greeting_bp.route('/home', methods=['GET', 'POST'])
 @greeting_bp.route('/', methods=['GET', 'POST'])
 def greeting():
-    if request.method=="POST":
+    if request.method == "POST":
         user_name = request.form['user_name']
         return render_template('home.html', user_name=user_name)
     return render_template('home.html')
